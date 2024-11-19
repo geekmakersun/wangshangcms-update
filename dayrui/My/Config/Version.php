6a4b8caa0b2a92950ac11788f6aa1ba2b1231274
孙柄晨 <sunbingchen@13aq.com>
2024-11-19 23:18:57 +0800

    增删改 2024-11-19 23:18:57

--- dayrui/My/Config/Version.php
+++ dayrui/My/Config/Version.php
@@ -3,7 +3,7 @@
 return [
 
     'id' => '11',
-    'name' => '君科云CMS',
+    'name' => '网商CMS',
     'version' => '4.6.3',
     'updatetime' => '2024-11-19 14:09:05',
     'downtime' => '2024-11-19 20:55:09',
